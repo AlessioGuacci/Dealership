8c87b8e48cfabc4e22c98186589b71252fc5e334
Alessio Guacci <alessioguacci@gmail.com>
2024-09-30 17:39:23 +0200

    UserDTO created

--- src/main/java/guacci/dealership/DTO/UserDTO.java
+++ src/main/java/guacci/dealership/DTO/UserDTO.java
@@ -1,4 +1,20 @@
 package guacci.dealership.DTO;
 
+import guacci.dealership.model.enums.RoleType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UserDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private RoleType role;
+
+
 }
