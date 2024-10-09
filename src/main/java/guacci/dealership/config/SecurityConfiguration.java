4959715825a3bb78276e72dd4a47adfb52a41e97
Alessio Guacci <alessioguacci@gmail.com>
2024-10-09 17:23:38 +0200

    Security Configurations

--- src/main/java/guacci/dealership/config/SecurityConfiguration.java
+++ src/main/java/guacci/dealership/config/SecurityConfiguration.java
@@ -3,9 +3,7 @@ package guacci.dealership.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
