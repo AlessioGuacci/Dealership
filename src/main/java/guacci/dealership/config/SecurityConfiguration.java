686f4b993e59226bca9ba782c7a23b10c562b771
Alessio Guacci <alessioguacci@gmail.com>
2024-12-04 19:32:38 +0100

    updated SecurityConfiguration

--- src/main/java/guacci/dealership/config/SecurityConfiguration.java
+++ src/main/java/guacci/dealership/config/SecurityConfiguration.java
@@ -1,21 +1,46 @@
 package guacci.dealership.config;
 
+import guacci.dealership.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-
+@EnableWebSecurity
 public class SecurityConfiguration{
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordConfig passwordConfig;
+
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(auth->auth.requestMatchers("/admin/**").hasRole("ADMIN").
-                requestMatchers("/employee/**").hasRole("EMPLOYEE").
-                requestMatchers("/customer/**").hasRole("CUSTOMER").anyRequest().authenticated()
+        http.authorizeHttpRequests(auth->auth
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/employee/**").hasRole("EMPLOYEE")
+                .requestMatchers("/customer/**").hasRole("CUSTOMER")
+                .anyRequest().authenticated()
         ).httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordConfig.passwordEncoder());
+        return authenticationManagerBuilder.build();
+
     }
 }
